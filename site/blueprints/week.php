f16b745f2d3655176ba8a645746ba7af98014dbc
Laurel Schwulst <laurel.schwulst@gmail.com>
2015-09-01 22:47:25 -0700

    current week

--- site/blueprints/week.php
+++ site/blueprints/week.php
@@ -12,14 +12,19 @@ fields:
     label: Dates
     type:  text
     width: 1/2
+  current:
+    label: Current
+    type: checkbox
+    text: Is this the current week?
+    width: 1/3
   project_start:
   	label: Projects starting?
   	type: text
-  	width: 1/2
+  	width: 1/3
   project_end:
     label: Projects ending?
     type: text
-    width: 1/2
+    width: 1/3
   text:
     label: Text
     type:  textarea
--- site/templates/calendar.php
+++ site/templates/calendar.php
@@ -12,7 +12,7 @@
             <?php foreach($page->children()->visible() as $week): ?>
             <li class="calendar-row">
                 <div class="title">
-                    <span class="week"><?php echo $week->title()->html() ?></span> ... <?php echo $week->dates()->html() ?>
+                    <span class="week"><?php echo $week->title()->html() ?></span><span class="days"> ... <?php echo $week->dates()->html() ?></span>
                     <div class="projects-start-end">
                         <?php if (!$week->project_end()->empty()): ?>
                             &#60;/<?php echo $week->project_end()->html() ?>&#62;
@@ -21,6 +21,11 @@
                             &#60;<?php echo $week->project_start()->html() ?>&#62;
                         <?php endif ?>
                     </div>
+                    <?php if($week->current() == '1'): ?>
+                    <div class="current-week">
+                        <img src="assets/images/arrows/arrow02.gif">
+                    </div>
+                    <?php endif ?>
                 </div>
                 <div class="body">
                     <?php echo $week->text()->kirbytext() ?>
