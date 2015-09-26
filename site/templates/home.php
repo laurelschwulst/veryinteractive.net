99777c51a6ec1fc88320f0af9b846524cbf2d250
Laurel Schwulst <laurel.schwulst@gmail.com>
2015-09-26 03:02:24 -0700

    added egg

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,6 +3,10 @@
 
 <div id="town"></div>
 
+<a href="workshops/name-generator">
+	<div id="naming-workshop"></div>
+</a>
+
 <div id="greeting" class="large">
 	<?php echo $page->text()->kirbytext() ?>
 </div>
