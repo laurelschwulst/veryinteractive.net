754c1ce601db69f5de0457808d0229a1a4bd50a7
Laurel Schwulst <laurel.schwulst@gmail.com>
2015-10-02 16:10:04 -0700

    update

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,6 +3,7 @@
 <head>
 
   <meta charset="utf-8" />
+  <link rel="shortcut icon" type="image/png" href="/assets/images/favicon.ico">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
--- site/templates/calendar.php
+++ site/templates/calendar.php
@@ -23,7 +23,7 @@
                     </div>
                     <?php if($week->current() == '1'): ?>
                     <div class="current-week">
-                        <img src="assets/images/arrows/arrow03.gif">
+                        <img src="assets/images/arrows/arrow08.gif">
                     </div>
                     <?php endif ?>
                 </div>
