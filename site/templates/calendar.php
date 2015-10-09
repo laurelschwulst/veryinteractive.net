1c6bd4264fba42d4d23ffa0da738e65d308102e6
Laurel Schwulst <laurel.schwulst@gmail.com>
2015-10-09 11:50:24 -0500

    update

--- site/templates/calendar.php
+++ site/templates/calendar.php
@@ -23,7 +23,7 @@
                     </div>
                     <?php if($week->current() == '1'): ?>
                     <div class="current-week">
-                        <img src="assets/images/arrows/arrow08.gif">
+                        <img src="assets/images/arrows/arrow09.gif">
                     </div>
                     <?php endif ?>
                 </div>
