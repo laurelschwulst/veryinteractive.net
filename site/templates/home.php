8ec2c45fce921bf5c1d309f4835136a14e4eb968
Laurel Schwulst <laurel.schwulst@gmail.com>
2015-10-20 21:51:04 -0700

    added homepage link to coding from life

--- site/templates/home.php
+++ site/templates/home.php
@@ -7,6 +7,10 @@
 	<div id="naming-workshop"></div>
 </a>
 
+<a href="content/coding-from-life/">
+	<div id="coding-from-life"></div>
+</a>
+
 <div id="greeting" class="large">
 	<?php echo $page->text()->kirbytext() ?>
 </div>
