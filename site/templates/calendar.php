cad3aa4eede2049d8e672f0e7fc3dcbd6fdaa8d1
Laurel Schwulst <laurel.schwulst@gmail.com>
2015-11-30 11:13:16 -0800

    updated arrow

--- site/templates/calendar.php
+++ site/templates/calendar.php
@@ -23,7 +23,7 @@
                     </div>
                     <?php if($week->current() == '1'): ?>
                     <div class="current-week">
-                        <img src="assets/images/arrows/arrow14.gif">
+                        <img src="assets/images/arrows/arrow17.gif">
                     </div>
                     <?php endif ?>
                 </div>
