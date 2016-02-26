d2578afb71efd771fe67c9dcd35bfcb208e6ff4e
Laurel Schwulst <laurel.schwulst@gmail.com>
2016-02-25 19:47:29 -0500

    update

--- site/templates/blog-post.php
+++ site/templates/blog-post.php
@@ -6,6 +6,9 @@
 	<div class="blog-post text">
 		<a href="<?php echo $page->parent()->url() ?>">
 			<div class="blog-date">
+				<?php $real_date = $page->date(); ?>
+				<?php echo date('F j', $real_date); ?><br>
+				<?php echo date('Y', $real_date); ?>
 			</div>
 		</a>
 		<h1><?php echo $page->title()->html() ?></h1>
