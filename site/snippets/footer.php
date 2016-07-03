098bfae312b277ecdb8263babaef9ae0423d430b
Laurel Schwulst <laurel.schwulst@gmail.com>
2016-07-03 11:53:05 -0400

    added summer to home

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,6 +2,7 @@
 
 	echo js('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js');
   	echo js('https://dl.dropboxusercontent.com/u/9299006/Script/global.js');
+  	echo js('assets/js/bouncer.js');
   	echo js('assets/js/script.js');
   	
 ?>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,6 +1,14 @@
 <?php snippet('header') ?>
 <?php snippet('menu') ?>
 
+<div id="bouncer-container">
+  <div id="bouncer">
+    <a href="http://summer2016.veryinteractive.net">
+    	<div style="height: 160px; width: 160px; display: inline-block; background: #FDEE2F; border-radius: 50%;" class="sun"></div>
+    </a>
+  </div>
+</div>
+
 <div id="town"></div>
 
 <div id="greeting" class="large">
