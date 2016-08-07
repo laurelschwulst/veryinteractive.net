4857b7c0631d587b0fea6429d036e3e4a9e74c08
Laurel Schwulst <laurel.schwulst@gmail.com>
2016-08-06 21:26:10 -0400

    removed bouncing sun

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,13 +1,13 @@
 <?php snippet('header') ?>
 <?php snippet('menu') ?>
 
-<div id="bouncer-container">
+<!-- <div id="bouncer-container">
   <div id="bouncer">
     <a href="http://summer2016.veryinteractive.net">
     	<div style="height: 160px; width: 160px; display: inline-block; background: #FDEE2F; border-radius: 50%;" class="sun"></div>
     </a>
   </div>
-</div>
+</div> -->
 
 <div id="town"></div>
 
